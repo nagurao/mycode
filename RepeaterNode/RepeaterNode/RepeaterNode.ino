1563d08c66c2ae1a3e71c5c4780c593697798741
Nagu <nagurao.basude@gmail.com>
2016-12-14 16:26:35 +0530

    Minor changes to Repeater Node.

--- RepeaterNode/RepeaterNode/RepeaterNode.ino
+++ RepeaterNode/RepeaterNode/RepeaterNode.ino
@@ -1,18 +1,28 @@
 #include <TimeAlarms.h>
 #include <TimeLib.h>
 #include <Time.h>
+#include <SPI.h>
+
+#define REPEATER_NODE
 
 #define MY_RADIO_NRF24
 #define MY_REPEATER_FEATURE
-#define MY_NODE_ID 6
+#define MY_NODE_ID BALCONY_REPEATER_NODE_ID
 
-#include <SPI.h>
-#include <MySensors.h>
+#define MY_LEDS_BLINKING_FEATURE
+#define MY_DEFAULT_LED_BLINK_PERIOD 300
+#define MY_WITH_LEDS_BLINKING_INVERSE
+
+#define MY_DEFAULT_ERR_LED_PIN 4  
+#define MY_DEFAULT_RX_LED_PIN  6  
+#define MY_DEFAULT_TX_LED_PIN  5  
+
 #include <MyNodes.h>
+#include <MySensors.h>
 #include <MyConfig.h>
 
-#define APPLICATION_NAME "Testing MYS 6"
-#define APPLICATION_VERSION "20Aug2016"
+#define APPLICATION_NAME "Balcony Repeater Node"
+#define APPLICATION_VERSION "13Dec2016"
 
 AlarmId heartbeatTimer;
 
